87b5b41b966a1097a2fba00141e3151a76e2d027
sfazli96 <sfazli96@gmail.com>
2023-05-25 13:44:17 -0700

    added draw elements and red color on the test_surface

--- game.py
+++ game.py
@@ -9,12 +9,17 @@ pygame.display.set_caption('Runner')
 # clock object helps with time and controlling the framerate
 clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100,200)) # width, height
+test_surface.fill('Red')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
     # draw all our elements
+    screen.blit(test_surface, (0, 0))
     # update everything
     pygame.display.update()
     clock.tick(60) # should not run faster than 60 frames per second in the while true loop
