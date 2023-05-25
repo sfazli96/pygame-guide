3cdad6a3f1853b4adf2dae4191aa25469a786c3e
sfazli96 <sfazli96@gmail.com>
2023-05-25 13:37:58 -0700

    clock object help with time and controlling framerate

--- game.py
+++ game.py
@@ -4,6 +4,10 @@ from sys import exit
 pygame.init()
 # display surface
 screen = pygame.display.set_mode((800, 400)) # width, height
+# update the title
+pygame.display.set_caption('Runner')
+# clock object helps with time and controlling the framerate
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
@@ -13,3 +17,4 @@ while True:
     # draw all our elements
     # update everything
     pygame.display.update()
+    clock.tick(60) # should not run faster than 60 frames per second in the while true loop
