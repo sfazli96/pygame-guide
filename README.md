c27650db45f0e46d87c300ae25f0af40bb16180a
sfazli96 <sfazli96@gmail.com>
2023-05-30 16:16:01 -0700

    finished displaying images, start on animation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pygame-guide
+
+
+# original video from clearcode: https://www.youtube.com/watch?v=AY9MnQ4x3zk&t=1796s 
--- game.py
+++ game.py
@@ -8,9 +8,13 @@ screen = pygame.display.set_mode((800, 400)) # width, height
 pygame.display.set_caption('Runner')
 # clock object helps with time and controlling the framerate
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50) # font type, font size
 
-test_surface = pygame.Surface((100,200)) # width, height
-test_surface.fill('Red')
+# test_surface = pygame.Surface((100,200)) # width, height
+# test_surface.fill('Red')
+sky_surface = pygame.image.load('graphics/sky.jpg')
+ground_surface = pygame.image.load('graphics/ground.jpg')
+text_surface = test_font.render('My Game', False, 'Black') # text info, anti-alias, color
 
 while True:
     for event in pygame.event.get():
@@ -19,7 +23,9 @@ while True:
             exit()
 
     # draw all our elements
-    screen.blit(test_surface, (0, 0))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 250))
+    screen.blit(text_surface, (300, 50))
     # update everything
     pygame.display.update()
     clock.tick(60) # should not run faster than 60 frames per second in the while true loop
