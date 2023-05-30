53c2c3c4ceb7d5a6b2a2f176a1705736c53e7999
sfazli96 <sfazli96@gmail.com>
2023-05-30 16:54:11 -0700

    pygame animation snail, next rectangles

--- game.py
+++ game.py
@@ -12,9 +12,12 @@ test_font = pygame.font.Font(None, 50) # font type, font size
 
 # test_surface = pygame.Surface((100,200)) # width, height
 # test_surface.fill('Red')
-sky_surface = pygame.image.load('graphics/sky.jpg')
-ground_surface = pygame.image.load('graphics/ground.jpg')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
 text_surface = test_font.render('My Game', False, 'Black') # text info, anti-alias, color
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -26,6 +29,10 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 250))
     screen.blit(text_surface, (300, 50))
+    screen.blit(snail_surface, (snail_x_pos, 250))
+    snail_x_pos -= 4
+    if snail_x_pos < -100:
+        snail_x_pos = 800
     # update everything
     pygame.display.update()
     clock.tick(60) # should not run faster than 60 frames per second in the while true loop
