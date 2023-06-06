1166732affd10053f0184b81fa497e5b192cb66c
sfazli96 <sfazli96@gmail.com>
2023-06-06 11:12:13 -0700

    draw rectangles and ellipse

--- game.py
+++ game.py
@@ -14,7 +14,8 @@ test_font = pygame.font.Font(None, 50) # font type, font size
 # test_surface.fill('Red')
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
-text_surface = test_font.render('My Game', False, 'Black') # text info, anti-alias, color
+score_surface = test_font.render('My Game', False, (64, 64, 64)) # text info, anti-alias, color
+score_rect = score_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_x_pos = 600
@@ -35,7 +36,11 @@ while True:
     # draw all our elements
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect) # 3 arguments display screen, color and score
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+    # pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50, 200, 100, 100))
+
+    screen.blit(score_surface, score_rect)
     snail_rect.x -= 4 # snail moves left
     if snail_rect.right <= 0:
         snail_rect.left = 800
