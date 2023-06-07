c2f1d47036cf7902c2bbeeb83b888ba560c081fe
sfazli96 <sfazli96@gmail.com>
2023-06-07 12:02:27 -0700

    display score in game over state

--- game.py
+++ game.py
@@ -1,6 +1,12 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surface = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
+    score_rect = score_surface.get_rect(center = (400, 50))
+    screen.blit(score_surface, score_rect)
+
 pygame.init()
 # display surface
 screen = pygame.display.set_mode((800, 400)) # width, height
@@ -10,13 +16,14 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50) # font type, font size
 game_active = True
+start_time = 0
 
 # test_surface = pygame.Surface((100,200)) # width, height
 # test_surface.fill('Red')
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
-score_surface = test_font.render('My Game', False, (64, 64, 64)) # text info, anti-alias, color
-score_rect = score_surface.get_rect(center = (400, 50))
+# score_surface = test_font.render('My Game', False, (64, 64, 64)) # text info, anti-alias, color
+# score_rect = score_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_x_pos = 600
@@ -42,15 +49,17 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active: # actual game, part of the game
         # draw all our elements
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
         # draw the rectangles, ellipses, etc
-        pygame.draw.rect(screen, '#c0e8ec', score_rect) # 3 arguments display screen, color and score
-        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
-        screen.blit(score_surface, score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect) # 3 arguments display screen, color and score
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        # screen.blit(score_surface, score_rect)
+        display_score()
 
         snail_rect.x -= 4 # snail moves left
         if snail_rect.right <= 0:
@@ -82,7 +91,7 @@ while True:
 
         # collision for game
         if snail_rect.colliderect(player_rect):
-            game_active = False
+            game_active = False # game over
             # pygame.quit()
             # exit()
     else: # some kind of intro/menu screen
