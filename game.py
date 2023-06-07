5b59533805fe65bf17015f9c6c78af52c87c386d
sfazli96 <sfazli96@gmail.com>
2023-06-07 12:43:39 -0700

    display score correctly and transform surfaces

--- game.py
+++ game.py
@@ -1,11 +1,15 @@
 import pygame
 from sys import exit
 
+# display the score
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
-    score_surface = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
-    score_rect = score_surface.get_rect(center = (400, 50))
-    screen.blit(score_surface, score_rect)
+    score_surface = test_font.render(f'Score: {current_time}', False, (64, 64, 64)) # create a new score rectangle surface
+    score_rect = score_surface.get_rect(center = (400, 50)) # create the rectangle
+    screen.blit(score_surface, score_rect) # now blit/show it
+    return current_time
+
+
 
 pygame.init()
 # display surface
@@ -15,8 +19,9 @@ pygame.display.set_caption('Runner')
 # clock object helps with time and controlling the framerate
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50) # font type, font size
-game_active = True
+game_active = False
 start_time = 0
+score = 0
 
 # test_surface = pygame.Surface((100,200)) # width, height
 # test_surface.fill('Red')
@@ -25,13 +30,28 @@ ground_surface = pygame.image.load('graphics/ground.png')
 # score_surface = test_font.render('My Game', False, (64, 64, 64)) # text info, anti-alias, color
 # score_rect = score_surface.get_rect(center = (400, 50))
 
+# loading the snail surface
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_x_pos = 600
 snail_rect = snail_surface.get_rect(bottomright = (600, 300))
 
+# Loading the player walk surface
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300))
 player_gravity = 0
+
+# Intro Screen
+player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rect = player_stand.get_rect(center = (400, 200))
+
+# Title from intro screen
+game_name = test_font.render('Pixel Runner', False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center = (400, 80))
+
+# game message of press space to run
+game_message = test_font.render('Press space to run', False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center = (400, 320))
 
 while True:
     for event in pygame.event.get():
@@ -59,7 +79,7 @@ while True:
         # pygame.draw.rect(screen, '#c0e8ec', score_rect) # 3 arguments display screen, color and score
         # pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
         # screen.blit(score_surface, score_rect)
-        display_score()
+        score = display_score()
 
         snail_rect.x -= 4 # snail moves left
         if snail_rect.right <= 0:
@@ -95,8 +115,17 @@ while True:
             # pygame.quit()
             # exit()
     else: # some kind of intro/menu screen
-        screen.fill('Yellow')
+        screen.fill((94, 129, 162))
+        screen.blit(player_stand, player_stand_rect)
 
+        score_message = test_font.render(f'Your Score: {score}', False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center = (400, 330))
+        screen.blit(game_name, game_name_rect)
+
+        if score == 0:
+            screen.blit(game_message, game_message_rect)
+        else:
+            screen.blit(score_message, score_message_rect)
     # update everything
     pygame.display.update()
     clock.tick(60) # should not run faster than 60 frames per second in the while true loop
